e2fe2d734f970125c0da623bd8b1b9a59b333f77
Saini-Yogesh <yogesh.saini4002@gmail.com>
2026-06-29 23:20:35 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { initSessionCache } from "./config/sessionCache.js";
-
-// Initialize session cache wrapper for all non-AI API routes
-initSessionCache();
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
